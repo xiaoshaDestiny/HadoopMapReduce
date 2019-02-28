39eab4796ee274675dff944bc4408796c7da3fa4
xuruibiao <2284420047@qq.com>
2019-02-28 10:55:07 +0800

    找共同好友的Mapreduce的案例

---
+++ src/friends/OneShareFriendsDriver.java
@@ -0,0 +1,32 @@
+package friends;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+public class OneShareFriendsDriver {
+	public static void main(String[] args) throws Exception {
+		// 1 获取 job 对象
+		Configuration configuration = new Configuration();
+		Job job = Job.getInstance(configuration);
+		// 2 指定 jar 包运行的路径
+		job.setJarByClass(OneShareFriendsDriver.class);
+		// 3 指定 map/reduce 使用的类
+		job.setMapperClass(OneShareFriendsMapper.class);
+		job.setReducerClass(OneShareFriendsReducer.class);
+		// 4 指定 map 输出的数据类型
+		job.setMapOutputKeyClass(Text.class);
+		job.setMapOutputValueClass(Text.class);
+		// 5 指定最终输出的数据类型
+		job.setOutputKeyClass(Text.class);
+		job.setOutputValueClass(Text.class);
+		// 6 指定 job 的输入原始所在目录
+		FileInputFormat.setInputPaths(job, new Path(args[0]));
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));
+		// 7 提交
+		boolean result = job.waitForCompletion(true);
+		System.exit(result?0:1);
+		}
+}
---
+++ src/friends/OneShareFriendsReducer.java
@@ -0,0 +1,18 @@
+package friends;
+
+import java.io.IOException;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class OneShareFriendsReducer extends Reducer<Text, Text, Text, Text> {
+	@Override
+	protected void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+		StringBuffer sb = new StringBuffer();
+		//1 Æ´½Ó
+		for (Text person : values) {
+			sb.append(person).append(",");
+		}
+		//2 Ð´³ö
+		context.write(key, new Text(sb.toString()));
+	}
+}
